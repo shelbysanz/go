03d308cc7056552251d807c83befd0d478261693
shelbysanz <shelbsgt500@gmail.com>
2025-06-22 17:30:29 -0400

    feat: adding nested structs

---
+++ structs/nested_structs.go
@@ -0,0 +1,29 @@
+package main
+
+type messageToSend struct {
+	message   string
+	sender    user
+	recipient user
+}
+
+type user struct {
+	name   string
+	number int
+}
+
+func canSendMessage(mToSend messageToSend) bool {
+	if mToSend.recipient.number <= 0 {
+		return false
+	}
+	if mToSend.sender.number <= 0 {
+		return false
+	}
+	if mToSend.recipient.name == "" {
+		return false
+	}
+	if mToSend.sender.name == "" {
+		return false
+	}
+	return true
+}
+
