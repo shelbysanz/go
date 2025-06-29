fc7428e654ebdf8fb0c11561634170af595e553d
Shelby Sanchez <shelby@bluearrowmail.com>
2025-06-29 18:05:44 -0400

    feat: adding formatting notes and changing format to %v verb

--- errors/error_interface.go
+++ errors/error_interface.go
@@ -9,8 +9,16 @@ type divideError struct {
 }
 
 func (d divideError) Error() string {
-	return fmt.Sprintf("can not divide %f by zero", d.dividend)
+	return fmt.Sprintf("can not divide %v by zero", d.dividend)
 }
+
+/*
+	%v : value in a default format
+	%+v : when printing structs, adds field names
+	%#v : Go-syntax repr of the value
+	%T : Go-syntax repr of the type of the value
+	%% : Literal percentage sign; Consumes no value
+*/
 
 func divide(dividend, divisor float64) (float64, error) {
 	if divisor == 0 {
