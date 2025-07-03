5b13a21417cf82dcf9c865badba3f665ff685207
shelbysanz <shelbsgt500@gmail.com>
2025-07-03 14:50:28 -0400

    feat: 8.19 message tagger exercise

---
+++ slices/message_tagger.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strings"
+)
+
+type sms struct {
+	id      string
+	content string
+	tags    []string
+}
+
+func tagMessages(messages []sms, tagger func(sms) []string) []sms {
+	var tags []string
+	for _, sms := range messages {
+		tags = tagger(sms)
+		if tags == nil {
+			tags = []string{}
+		}
+		sms.tags = tags
+	}
+	return messages
+}
+
+func tagger(msg sms) []string {
+	tags := []string{}
+	if strings.Contains(strings.ToLower(msg.content), "urgent") {
+		tags = append(tags, "Urgent")
+	}
+	if strings.Contains(strings.ToLower(msg.content), "sale") {
+		tags = append(tags, "Promo")
+	}
+	return tags
+}
