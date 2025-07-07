b5cd243f3ff764f4952e96c96852e4483a507d35
shelbysanz <shelbsgt500@gmail.com>
2025-07-07 15:13:29 -0400

    feat: exercise 10.6 checking a pointer is not nil before dereferencing

---
+++ pointers/nil_pointers.go
@@ -0,0 +1,17 @@
+
+package main
+
+import (
+	"strings"
+)
+
+func removeProfanity(message *string) {
+	if message == nil {
+		return
+	}
+	messageVal := *message
+	messageVal = strings.ReplaceAll(messageVal, "fubb", "****")
+	messageVal = strings.ReplaceAll(messageVal, "shiz", "****")
+	messageVal = strings.ReplaceAll(messageVal, "witch", "*****")
+	*message = messageVal
+}
