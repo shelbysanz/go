f91e7af149fd76ad96afd18179841445bda3bd61
shelbysanz <shelbsgt500@gmail.com>
2025-07-11 22:27:51 -0400

    feat: exercise 15.1 lack of enums - using fmt.Errorf

---
+++ enums/lack_of_enums.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func (a *analytics) handleEmailBounce(em email) error {
+	err := em.recipient.updateStatus(em.status)
+	if err != nil {
+		return fmt.Errorf("error updating user status: %w", err)
+	}
+	a.track(em.status)
+	if err != nil {
+		return fmt.Errorf("error tracking user bounce: %w", err)
+	}
+	return nil
+}
