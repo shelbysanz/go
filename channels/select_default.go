13401fbd55cf9d78786ab5e0fc5acb708957b6f5
shelbysanz <shelbsgt500@gmail.com>
2025-07-11 21:10:52 -0400

    feat: exercise 12.8 select default case - using the default to log when waiting - using time libray to Sleep() and to use Milliseconds

---
+++ channels/select_default.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"time"
+)
+
+func saveBackups(snapshotTicker, saveAfter <-chan time.Time, logChan chan string) {
+	for {
+		select {
+		case _, ok := <-snapshotTicker:
+			if !ok {
+				return
+			}
+			takeSnapshot(logChan)
+		case _, ok := <-saveAfter:
+			if !ok {
+				return
+			}
+			saveSnapshot(logChan)
+			return
+		default:
+			waitForData(logChan)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
+// don't touch below this line
+
+func takeSnapshot(logChan chan string) {
+	logChan <- "Taking a backup snapshot..."
+}
+
+func saveSnapshot(logChan chan string) {
+	logChan <- "All backups saved!"
+	close(logChan)
+}
+
+func waitForData(logChan chan string) {
+	logChan <- "Nothing to do, waiting..."
+}
