0ad4610f503e900a9432e5a572e3f24f14ad5ff4
shelbysanz <shelbsgt500@gmail.com>
2025-07-13 18:10:39 -0400

    feat: exercise 1.1 communicating on the web

---
+++ go-http-clients/http/retrieve.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	issues, err := getIssueData()
+	if err != nil {
+		log.Fatalf("error getting issue data: %v", err)
+	}
+
+	// Don't edit above this line
+
+	fmt.Println(string(issues))
+}
