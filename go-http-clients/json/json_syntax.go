9a49f6fc809b4b46f59380e425555808aacec526
Shelby Sanchez <shelby@bluearrowmail.com>
2025-07-14 16:43:51 -0400

    feat: exercise 2.1 json syntax

---
+++ go-http-clients/json/json_syntax.go
@@ -0,0 +1,27 @@
+package main
+
+const issueList = `
+	[
+		{
+			"id": 0,
+			"name": "Fix the thing",
+			"estimate": 0.5,
+			"completed": false
+		},
+		{
+			"id": 1,
+			"name": "Unstick the widget",
+			"estimate": 30,
+			"completed": false
+		}
+	]
+`
+
+const userObject = `
+	{
+		"name": "Wayne Lagner",
+		"role": "Developer",
+		"remote": true
+	}
+
+`
