5f8a12cf9ec193a0e88c780901928599ad22ffc2
Shelby Sanchez <shelby@bluearrowmail.com>
2025-07-15 16:52:21 -0400

    feat: exercise 3.1 web addresses - makes request to get ip addresses of websites - unmarshals the json response

---
+++ go-http-clients/dns/web_addresses.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type (
+	DNSResponse struct {
+		Status   int        `json:"Status"`
+		Tc       bool       `json:"TC"`
+		Rd       bool       `json:"RD"`
+		Ra       bool       `json:"RA"`
+		Ad       bool       `json:"AD"`
+		Cd       bool       `json:"CD"`
+		Question []Question `json:"Question"`
+		Answer   []Answer   `json:"Answer"`
+	}
+	Question struct {
+		Name string `json:"name"`
+		Type int    `json:"type"`
+	}
+	Answer struct {
+		Name string `json:"name"`
+		Type int    `json:"type"`
+		TTL  int    `json:"TTL"`
+		Data string `json:"data"`
+	}
+)
+
+func getIPAddress(domain string) (string, error) {
+	url := fmt.Sprintf("https://1.1.1.1/dns-query?name=%s&type=A", domain)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Set("accept", "application/dns-json")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error sending request: %w", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var unmarshalled DNSResponse
+	if err := json.Unmarshal(body, &unmarshalled); err != nil {
+		return "", err
+	}
+
+	if len(unmarshalled.Answer[0].Data) == 0 {
+		return "", errors.New("Answers is empty!")
+	}
+
+	return unmarshalled.Answer[0].Data, nil
+}
+
+func main() {
+	fmt.Println(getIPAddress("boot.dev"))
+}
